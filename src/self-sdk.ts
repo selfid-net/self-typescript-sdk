436897bb4a1b6a1c7d0451b1b63172a5f2694e13
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2020-07-15 08:40:25 +0200

    Self urls calculations
    Calculate the urls based on the env input and / or baseURL / messagingURL

--- src/self-sdk.ts
+++ src/self-sdk.ts
@@ -1,15 +1,30 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-export default class SelfSDK {
+  export default class SelfSDK {
     appID: string;
     appKey: string;
     storageKey: string;
+    baseURL: string;
+    messagingURL: string;
+    autoReconnect: boolean;
 
-    constructor(appID: string, appKey: string, storageKey: string) {
+    defaultBaseURL = "https://api.selfid.net";
+    defaultMessagingURL = "wss://messaging.selfid.net/v1/messaging"
+
+    constructor(
+        appID: string,
+        appKey: string,
+        storageKey: string,
+        opts?: { baseURL?: string, messagingURL?: string, env?: string, autoReconnect?: boolean },
+    ) {
         this.appID = appID;
         this.appKey = appKey;
         this.storageKey = storageKey;
+
+        this.baseURL = this.calculateBaseURL(opts)
+        this.messagingURL = this.calculateMessagingURL(opts)
+        this.autoReconnect = opts?.autoReconnect ? opts?.autoReconnect : true;
     }
 
     authentication() {
@@ -30,5 +45,27 @@ export default class SelfSDK {
     messaging() {
         console.log("messaging")
         return true
+    }
+
+    private calculateBaseURL(opts?: { baseURL?: string, env?: string }) {
+        if(opts?.baseURL) {
+            return opts?.baseURL
+        }
+        if(opts?.env) {
+            return `https://api.${opts?.env}.selfid.net`
+        }
+
+        return this.defaultBaseURL
+    }
+
+    private calculateMessagingURL(opts?: { messagingURL?: string, env?: string }) {
+        if(opts?.messagingURL) {
+            return opts?.messagingURL
+        }
+        if(opts?.env) {
+            return `wss://messaging.${opts?.env}.selfid.net/v1/messaging`
+        }
+
+        return this.defaultMessagingURL
     }
 }
--- test/self-sdk.test.ts
+++ test/self-sdk.test.ts
@@ -31,4 +31,26 @@ describe("SelfSDK test", () => {
     const sdk = new SelfSDK("appId", "appKey", "storageKey")
     expect(sdk.messaging()).toBeTruthy()
   })
+
+  it("default urls point to production", () => {
+    const sdk = new SelfSDK("appId", "appKey", "storageKey")
+    expect(sdk.baseURL).toEqual(sdk.defaultBaseURL)
+    expect(sdk.messagingURL).toEqual(sdk.defaultMessagingURL)
+  })
+
+  it("urls vary for each environment", () => {
+    const sdk = new SelfSDK("appId", "appKey", "storageKey", {env: "review"})
+    expect(sdk.baseURL).toEqual(`https://api.review.selfid.net`)
+    expect(sdk.messagingURL).toEqual(`wss://messaging.review.selfid.net/v1/messaging`)
+
+    const sdkSandbox = new SelfSDK("appId", "appKey", "storageKey", {env: "sandbox"})
+    expect(sdkSandbox.baseURL).toEqual(`https://api.sandbox.selfid.net`)
+    expect(sdkSandbox.messagingURL).toEqual(`wss://messaging.sandbox.selfid.net/v1/messaging`)
+  })
+
+  it("forced urls take prevalence", () => {
+    const sdk = new SelfSDK("appId", "appKey", "storageKey", {env: "review", baseURL: "http://localhost", messagingURL:"ws://localhost"})
+    expect(sdk.baseURL).toEqual("http://localhost")
+    expect(sdk.messagingURL).toEqual("ws://localhost")
+  })
 })
