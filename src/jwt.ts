d36c0e21f020b127f770e1e1528570a5db490f84
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2020-07-16 12:13:29 +0200

    jwt

--- src/jwt.ts
+++ src/jwt.ts
@@ -1,34 +1,69 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
 const _sodium = require('libsodium-wrappers');
 
 export default class Jwt {
     appID: string
     appKey: string
+    sodium: any
+    keypair: any
 
     constructor(appID: string, appKey: string) {
         this.appID = appID
         this.appKey = appKey;
     }
 
-    do() {
-        (async() => {
-            await _sodium.ready;
-            const sodium = _sodium;
-            console.log("booo");
+    private async load() {
+        if(this.sodium) {
+            return
+        }
+        await _sodium.ready;
+        this.sodium = _sodium;
+        let seed = this.sodium.from_base64(this.appKey)
+        this.keypair = this.sodium.crypto_sign_seed_keypair(seed)
+    }
 
-            let seed = sodium.from_base64(this.appKey)
-            console.log(".....")
-            console.log(seed.toString())
-            console.log(".....")
+    async authToken() {
+        let header = await this.header()
+        let fiveMins = 5*60*1000
+        let anHour = 60*60*1000
 
-            let keypair = sodium.crypto_sign_ed25519_pk_to_curve25519(seed)
+        // TODO : timestamps must be calculated within a NTP server
+        let body = await this.encode(JSON.stringify({
+            jti: uuidv4(),
+            iat: Math.floor(Date.now() - fiveMins / 1000),
+            exp: Math.floor(Date.now() + anHour / 1000),
+            iss: this.appID
+        }))
 
-            console.log("++++")
-            console.log(keypair.toString())
-            console.log("++++")
+        let payload = `${header}.${body}`
+        let signature = await this.sign(header + body)
 
-            console.log("end");
-            });
+        return `${payload}.${signature}`
+    }
 
+    private async sign(input: string) {
+        try {
+            await this.load()
+            let signature = this.sodium.crypto_sign(input, this.keypair.privateKey)
+            return this.sodium.to_base64(signature, this.sodium.base64_variants.URLSAFE_NO_PADDING)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    private async header() {
+        return this.encode(`{"alg":"EdDSA","typ":"JWT"}`)
+    }
+
+    private async encode(input: string) {
+        try {
+            await this.load()
+            return this.sodium.to_base64(input, this.sodium.base64_variants.URLSAFE_NO_PADDING)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
 
--- test/jwt.test.ts
+++ test/jwt.test.ts
@@ -4,12 +4,17 @@ import Jwt from '../src/jwt';
  * Jwt test
  */
 describe("lol", () => {
-    it("works", () => {
-        let appID = "id"
-        let appKey = "key"
+    it("works", async() => {
+        let jwt = new Jwt("6d0c13fa84fb4f6dcc0ac66c9b230bff", "cTPCVQEWBpJoWjIcNqchT9hLwCCmg47dvlXjra3vo3E");
 
-        let jwt = new Jwt(appID, appKey)
-
-        jwt.do()
+        let t = await jwt.authToken()
+        console.log(".......")
+        console.log(".......")
+        console.log(".......")
+        console.log(t)
+        console.log(".......")
+        console.log(".......")
+        console.log(".......")
+        console.log(".......")
     })
 })
