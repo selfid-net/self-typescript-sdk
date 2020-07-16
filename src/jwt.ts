a0c48c50f1e5ca7ada95d3cc1152add755fc2f9b
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2020-07-16 15:48:07 +0200

    verify

--- src/jwt.ts
+++ src/jwt.ts
@@ -14,15 +14,6 @@ export default class Jwt {
         this.appKey = appKey;
     }
 
-    private async load() {
-        if(this.sodium) {
-            return
-        }
-        await _sodium.ready;
-        this.sodium = _sodium;
-        let seed = this.sodium.from_base64(this.appKey)
-        this.keypair = this.sodium.crypto_sign_seed_keypair(seed)
-    }
 
     async authToken() {
         let header = await this.header()
@@ -30,20 +21,21 @@ export default class Jwt {
         let anHour = 60*60*1000
 
         // TODO : timestamps must be calculated within a NTP server
-        let body = await this.encode(JSON.stringify({
+        let jsonBody = JSON.stringify({
             jti: uuidv4(),
-            iat: Math.floor(Date.now() - fiveMins / 1000),
-            exp: Math.floor(Date.now() + anHour / 1000),
+            iat: Math.floor((new Date().getTime() - fiveMins) / 1000),
+            exp: Math.floor((new Date().getTime() + anHour) / 1000),
             iss: this.appID
-        }))
+        })
+        let body = await this.encode(jsonBody)
 
         let payload = `${header}.${body}`
-        let signature = await this.sign(header + body)
+        let signature = await this.sign(payload)
 
         return `${payload}.${signature}`
     }
 
-    private async sign(input: string) {
+    async sign(input: string) {
         try {
             await this.load()
             let signature = this.sodium.crypto_sign_detached(input, this.keypair.privateKey)
@@ -51,6 +43,29 @@ export default class Jwt {
         } catch (error) {
             console.log(error)
         }
+    }
+
+    async verify(input: string, pk: any) {
+        try {
+            await this.load()
+            let parts = input.split(".")
+            let msg = `${parts[0]}.${parts[1]}`
+            let sig = this.sodium.from_base64(parts[2], this.sodium.base64_variants.URLSAFE_NO_PADDING)
+            return this.sodium.crypto_sign_verify_detached(sig, msg, pk)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    private async load() {
+        if(this.sodium) {
+            return
+        }
+        await _sodium.ready;
+        this.sodium = _sodium;
+
+        let seed = this.sodium.from_base64(this.appKey, this.sodium.base64_variants.ORIGINAL_NO_PADDING)
+        this.keypair = this.sodium.crypto_sign_seed_keypair(seed)
     }
 
     private async header() {
@@ -65,6 +80,4 @@ export default class Jwt {
             console.log(error)
         }
     }
-
-
 }
--- test/jwt.test.ts
+++ test/jwt.test.ts
@@ -5,16 +5,12 @@ import Jwt from '../src/jwt';
  */
 describe("lol", () => {
     it("works", async() => {
-        let jwt = new Jwt("6d0c13fa84fb4f6dcc0ac66c9b230bff", "cTPCVQEWBpJoWjIcNqchT9hLwCCmg47dvlXjra3vo3E");
+        let jwt = new Jwt("738a9ba67b14520a2282a892efd8f696", "IWtkmYLTLq+yhVaFnXOetjuOAF5pv3J7QkfdP00EeZA");
 
         let t = await jwt.authToken()
-        console.log(".......")
-        console.log(".......")
-        console.log(".......")
-        console.log(t)
-        console.log(".......")
-        console.log(".......")
-        console.log(".......")
-        console.log(".......")
+        console.log(`curl -X GET -H 'Accept: application/json' -H "Authorization: Bearer ${t}" https://api.review.selfid.net/v1/identities/48931618754/devices`)
+
+        let verified = await jwt.verify(t, jwt.keypair.publicKey)
+        expect(verified).toBeTruthy()
     })
 })
