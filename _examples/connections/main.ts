9cec5c509722d082a9df4df72a3651c2c4153c50
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2020-07-22 22:33:55 +0200

    fix example

--- _examples/connections/main.ts
+++ _examples/connections/main.ts
@@ -1,8 +1,8 @@
-async function manageConnections(appID, appSecret, connection) {
+async function manageConnections(appID: string, appSecret: string, connection: string) {
     const SelfSDK = require("../../src/self-sdk.ts");
-    const sdk = await SelfSDK.build( "109a21fdd1bfaffa2717be1b4edb57e9", "RmfQdahde0n5SSk1iF4qA2xFbm116RNjjZe47Swn1s4", "random");
+    const sdk = await SelfSDK.build( appID, appSecret, "random");
 
-    let success = await sdk.messaging().permitConnection("35918759412")
+    let success = await sdk.messaging().permitConnection(connection)
     if(!success) {
         throw new Error("problem permitting connection")
     }
@@ -13,7 +13,7 @@ async function manageConnections(appID, appSecret, connection) {
     console.log(connections)
 
 
-    success = await sdk.messaging().revokeConnection("35918759412")
+    success = await sdk.messaging().revokeConnection(connection)
     if(!success) {
         throw new Error("problem revoking connection")
     }
